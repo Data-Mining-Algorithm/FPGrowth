0dd4665494a144fcb8530082275658779652241f
aly4080 <shadow777788@gmail.com>
2024-11-22 19:25:46 +0200

    progess done in fpgrowth.py

--- Src/FpGrowth.py
+++ Src/FpGrowth.py
@@ -12,9 +12,36 @@ Description of the function's purpose.
     Hint: Use python
 """
 
+from FpTree import get_path_to_root
 
 def FpGrowth(FpTree, min_sup):
-    return
+    if not FpTree.item_list: # if nothing is inside the tree's item list then exit
+        return []
+    
+    frequent_itemsets = []
+
+    for item in FpTree.item_list: 
+        nodes = FpTree.occurrences.get(item, []) # get the list of nodes where the current items appears in the tree
+        
+        support = 0
+        for node in nodes:
+            support += node.count # add the number of nodes to know the support of the current item
+
+        if support < min_sup: # if the current item's support is less than the minimum support then go back to the start of the loop and start with the next item
+            continue
+
+        frequent_itemsets.append(([item], support))
+
+        conditional_transactions = [] # empty list to store the conditional transactions
+
+        for node in nodes:
+            path_to_root = get_path_to_root(node)
+            path_to_root = [n.item for n in reversed(path_to_root[:-1])] # reverse the path + remove the current item from it
+            conditional_transactions.append(path_to_root)
+
+            #TBD  
+
+    return frequent_itemsets
 
 
 """
@@ -27,5 +54,6 @@ Description of the function's purpose.
 """
 
 
-def print_frequent_itemsets(itemset):
-    return
+def print_frequent_itemsets(itemsets):
+    for itemset, count in itemsets:
+        print(f"Itemset: {itemset}, Count: {count}")
--- Src/main.py
+++ Src/main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from FpTree import build_tree
+from FpGrowth import FpGrowth
 
 min_sup = 0.2
 min_conf = 0.5
@@ -7,10 +9,15 @@ min_conf = 0.5
 # occurrences = {}  # (item : list of nodes where the item appeared at the main trie)
 frequent_itemsets = {}  # (itemset : frequent_count)
 
-file_path = "D:/Curriculum/Semester 5/Data Mining/Fpgrowth_Project/transactions.xlsx" # replace with complete file path
+file_path = "D:/FPGrowth/transactions.xlsx" # replace with complete file path
 data = pd.read_excel(file_path)
 transactions = data['items'].apply(lambda x: x.split(","))  # list of lists
 transactions = [list(set([item.strip() for item in transaction])) for transaction in transactions]  # make unique
 frequent_count = round(min_sup * len(transactions))
-# print(transactions)
+tree = build_tree(transactions, frequent_count)
+frequent_itemsets = FpGrowth(tree, frequent_count)
+for itemset, support in frequent_itemsets:
+    print(f"Itemset: {itemset}, Support: {support}")
+
+
 
