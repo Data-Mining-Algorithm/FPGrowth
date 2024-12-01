c265e52e8649ce7e2d3c5b2afa63d5814f87213f
Mahmoud Bassem <82619789+Mahmoud-Bassem@users.noreply.github.com>
2024-12-01 13:34:54 +0200

    Fixed logic errors in FpGrowth

--- Src/FpGrowth.py
+++ Src/FpGrowth.py
@@ -12,44 +12,37 @@ Description of the function's purpose.
     Hint: Use python
 """
 
-from FpTree import build_tree,get_path_to_root,Node,Tree
+from FpTree import build_tree, get_path_to_root, Node, Tree
 
-def FpGrowth(tree: Tree, min_sup):
 
-    if not tree.item_list: # if nothing is inside the tree's item list then exit
+def FpGrowth(tree, min_sup):
+    if not tree.item_list:  # if nothing is inside the tree's item list then exit
         return []
 
-    frequent_itemsets = []
+    frequent_itemsets = {} # {itemset, frequency)
 
-    for item in tree.item_list: 
-        nodes = tree.occurrences.get(item, []) # get the list of nodes where the current items appears in the tree
-        
+    for item in tree.item_list:
+        nodes = tree.occurrences.get(item, [])  # get the list of nodes where the current items appears in the tree
+
         support = 0
         for node in nodes:
-            support += node.count # add the number of nodes to know the support of the current item
+            support += node.count  # add the number of nodes to know the support of the current item
 
-        if support < min_sup: # if the current item's support is less than the minimum support then go back to the start of the loop and start with the next item
+        if support < min_sup:  # if the current item's support is less than the minimum support then go back to the start of the loop and start with the next item
             continue
 
-        frequent_itemsets.append(([item], support))
+        frequent_itemsets[item] = support
 
-        conditional_transactions = [] # empty list to store the conditional transactions
+        paths = []  # empty list to store the pattern paths
 
         for node in nodes:
             path_to_root = get_path_to_root(node)
             path_to_root = [n.item for n in reversed(path_to_root[:-1])]
-            conditional_transactions.append(path_to_root)
+            paths.append(path_to_root)
 
-        print(f"Conditional Transactions for {item}: {conditional_transactions}")
+        conditional_tree = build_tree(paths, min_sup, tree.conditional_items, item)
 
-        conditional_tree = build_tree(conditional_transactions, min_sup)
-
-        print(f"Conditional Tree for {item}: {conditional_tree.item_list}")
-
-        conditional_frequent_itemsets = FpGrowth(conditional_tree, min_sup)
-
-        for itemset, count in conditional_frequent_itemsets:
-            frequent_itemsets.append(([item] + itemset, count))
+        frequent_itemsets.update(FpGrowth(conditional_tree, min_sup))
 
     return frequent_itemsets
 
